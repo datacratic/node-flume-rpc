de6161224cf907ecf4c2793cb066fcf6f394235d
jeremybarnes <jeremy@recoset.com>
2011-09-11 23:47:40 -0400

    make the tests work again

--- test/flume_rpc_test.js
+++ test/flume_rpc_test.js
@@ -1,8 +1,6 @@
 var sys = require('sys'),
-Sink = require('flume-rpc-sink').Sink,
-Source = require('flume-rpc-sink').Source;
-
-console.log(sys.inspect(require('flume-rpc-sink')));
+Sink = require('flume-rpc').Sink,
+Source = require('flume-rpc').Source;
 
 var source;
 var sink = new Sink();
