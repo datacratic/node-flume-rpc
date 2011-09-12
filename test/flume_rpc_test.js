79af7e91c78248c63dba2bde3223eb148e293ee9
jeremybarnes <jeremy@recoset.com>
2011-09-11 23:16:22 -0400

    basic use case is working

--- test/flume_rpc_test.js
+++ test/flume_rpc_test.js
@@ -1,7 +1,32 @@
 var sys = require('sys'),
-    Sink = require('flume-rpc-sink').Sink;
+Sink = require('flume-rpc-sink').Sink,
+Source = require('flume-rpc-sink').Source;
+
+console.log(sys.inspect(require('flume-rpc-sink')));
 
 var sink = new Sink();
-sink.on('message', function (msg) { console.log(sys.inspect(msg)); });
-sink.listen(9090);
+sink.on('message', function (msg) { console.log('got message: ',
+                                                sys.inspect(msg)); });
+var source;
+
+
+sink.on('listening', onListenDone);
+sink.listen(9090, onListenDone);
+
+function onListenDone()
+{
+    console.log('onListenDone()');
+    source = new Source('localhost', 9090);
+    source.on('connect', doSend);
+}
+
+function doSend()
+{
+    function doneSend(err)
+    {
+        console.log('done logging: ' + sys.inspect(err));
+    }
+
+    source.log('hello', doneSend);
+}
 
